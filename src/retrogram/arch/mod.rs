3b63a3763950fbf4f83dcd4da9828f55b672f7b7
David Wendt <dcrkid@yahoo.com>
2019-04-07 20:33:26 -0400

    Add enumerations for the platforms and architectures we support.

--- src/retrogram/arch/mod.rs
+++ src/retrogram/arch/mod.rs
@@ -1,1 +1,22 @@
+//! Implementations of processor architectures that Retrogram can analyze.
+
 pub mod lr35902;
+
+use std::str;
+
+/// Enumeration of all architectures that ship with Retrogram.
+enum ArchName {
+    LR35902
+}
+
+impl str::FromStr for ArchName {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_ascii_lowercase().as_ref() {
+            "LR35902" => Ok(ArchName::LR35902),
+            "GBZ80" => Ok(ArchName::LR35902),
+            _ => Err(())
+        }
+    }
+}
--- src/retrogram/platform/mod.rs
+++ src/retrogram/platform/mod.rs
@@ -1,1 +1,24 @@
+//! Implementations of full platforms which define the entire execution
+//! environment that a program image will be run in.
+//! 
+//! A properly configured platform is required in order to analyze a program.
+
 pub mod gb;
+
+use std::str;
+
+/// Enumeration of all platforms that ship with Retrogram.
+enum PlatformName {
+    GB
+}
+
+impl str::FromStr for PlatformName {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_ascii_lowercase().as_ref() {
+            "GB" => Ok(PlatformName::GB),
+            _ => Err(())
+        }
+    }
+}
