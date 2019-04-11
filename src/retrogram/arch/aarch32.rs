489e0808f7503b400c2e9815e97d6fd045e49364
David Wendt <dcrkid@yahoo.com>
2019-04-11 16:48:59 -0600

    First stab at eventual ARM support

---
+++ src/retrogram/arch/aarch32.rs
@@ -0,0 +1,40 @@
+//! ARM, formerly an acronym of Acorn RISC Machine, and a quite popular ISA.
+//! 
+//! This only covers 32-bit ARM, now known as AArch32.
+
+use crate::retrogram::{memory, ast, reg};
+
+enum Aarch32Register {
+    R0, R1, R2, R3,
+    R4, R5, R6, R7,
+    R8, R9, R10, R11,
+    R12, R13, R14, R15,
+}
+
+/// The type which represents a value contained in an ARM AArch32 register.
+type Value = u32;
+
+/// The type which represents an ARM AArch32 memory address.
+type Pointer = u32;
+
+/// The type which represents a positive memory offset.
+type Offset = u32;
+
+/// The type which represents data stored in memory as seen by the processor.
+type Data = u8;
+
+/// The compatible memory model type necessary to analyze AArch32 programs.
+pub type Bus = memory::Memory<Pointer, Data, Offset>;
+
+/// Disassemble the instruction at `p` in `mem`.
+/// 
+/// This function returns:
+/// 
+///  * A string representation of the instruction encountered, if there is a
+///    valid instruction at P; otherwise `None`
+///  * The size of the current instruction
+///  * True, if execution would continue at the instruction following this one,
+///    or false if the instruction terminates the current basic block
+pub fn disassemble(p: Pointer, mem: &Bus, ctx: &reg::Context) -> (Option<ast::Instruction>, Offset, bool) {
+    (None, 0, false)
+}
--- src/retrogram/arch/mod.rs
+++ src/retrogram/arch/mod.rs
@@ -1,12 +1,14 @@
 //! Implementations of processor architectures that Retrogram can analyze.
 
 pub mod lr35902;
+pub mod aarch32;
 
 use std::str;
 
 /// Enumeration of all architectures that ship with Retrogram.
 pub enum ArchName {
-    LR35902
+    LR35902,
+    AARCH32,
 }
 
 impl str::FromStr for ArchName {
@@ -14,8 +16,11 @@ impl str::FromStr for ArchName {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.to_ascii_lowercase().as_ref() {
-            "LR35902" => Ok(ArchName::LR35902),
-            "GBZ80" => Ok(ArchName::LR35902),
+            "lr35902" => Ok(ArchName::LR35902),
+            "gbz80" => Ok(ArchName::LR35902),
+            "arm" => Ok(ArchName::AARCH32),
+            "arm32" => Ok(ArchName::AARCH32),
+            "aarch32" => Ok(ArchName::AARCH32),
             _ => Err(())
         }
     }
