f8cbe5d05791051af8c6889061da59c9e610c28c
David Wendt <dcrkid@yahoo.com>
2019-05-31 12:45:45 -0600

    Break out the analysis further into a function that can be called multiple times for a future recursive analysis routine

--- src/retrogram/cli/scan.rs
+++ src/retrogram/cli/scan.rs
@@ -5,40 +5,15 @@ use std::collections::HashSet;
 use std::fmt::Debug;
 use crate::retrogram::{project, platform, arch, asm, ast, input, analysis, memory, cli};
 
-/// Given a program, analyze a given routine with the given memory model and
-/// disassembler and populate the database with the results.
-/// 
-/// This function exists to isolate as much as possible of the top-level scan
-/// implementation from generic typing concerns. You call it with a compatible
-/// disassembler and memory and the database gets updated as you wished.
-/// 
-/// TODO: The current set of lifetime bounds preclude the use of zero-copy
-/// deserialization. We should figure out a way around that.
-fn scan_for_arch<I, SI, F, P, MV, S, IO, DIS, IMP>(prog: &project::Program, start_spec: &str, disassembler: &DIS, bus: &memory::Memory<P, MV, S, IO>, importer: Option<&IMP>) -> io::Result<()>
-    where for <'dw> P: memory::PtrNum<S> + analysis::Mappable + cli::Nameable + serde::Deserialize<'dw> + serde::Serialize,
-        for <'dw> S: memory::Offset<P> + Debug + serde::Deserialize<'dw> + serde::Serialize,
-        DIS: Fn(&memory::Pointer<P>, &memory::Memory<P, MV, S, IO>) -> (Option<ast::Instruction<I, SI, F, P>>, S, bool, bool, Vec<analysis::Reference<P>>),
-        IMP: Fn(io::BufReader<fs::File>, &mut analysis::Database<P, S>) -> io::Result<()> {
-    let image = prog.iter_images().next().ok_or(io::Error::new(io::ErrorKind::Other, "Did not specify an image"))?;
-    let mut file = fs::File::open(image)?;
-
-    let mut pjdb = match project::ProjectDatabase::read(prog.as_database_path()) {
-        Ok(pjdb) => pjdb,
-        Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
-            eprintln!("Creating new database for project");
-            project::ProjectDatabase::new()
-        },
-        Err(e) => return Err(e)
-    };
-
-    let mut db = pjdb.get_database_mut(prog.as_name().expect("Projects must be named!"));
-    db.update_indexes();
-
-    if let Some(importer) = importer {
-        db.import_symbols(prog, importer)?;
-    }
-
-    let start_pc = input::parse_ptr(start_spec, db, bus).expect("Must specify a valid address to analyze");
+/// Scan a specific starting PC and add the results of the analysis to the
+/// database.
+fn scan_pc_for_arch<I, SI, F, P, MV, S, IO, DIS>(db: &mut analysis::Database<P, S>, start_pc: &memory::Pointer<P>,
+        disassembler: &DIS,
+        bus: &memory::Memory<P, MV, S, IO>) -> io::Result<()>
+    where P: memory::PtrNum<S> + analysis::Mappable + cli::Nameable,
+        S: memory::Offset<P>,
+        DIS: Fn(&memory::Pointer<P>, &memory::Memory<P, MV, S, IO>) -> (Option<ast::Instruction<I, SI, F, P>>, S, bool,
+            bool, Vec<analysis::Reference<P>>) {
     let (orig_asm, xrefs, pc_offset, blocks) = analysis::disassemble_block(start_pc.clone(), bus, disassembler)?;
     
     for block in blocks {
@@ -72,7 +47,47 @@ fn scan_for_arch<I, SI, F, P, MV, S, IO, DIS, IMP>(prog: &project::Program, star
             db.insert_placeholder_label(line.source_address().clone(), analysis::ReferenceKind::Unknown);
         },
         _ => {}
+    };
+
+    Ok(())
+}
+
+/// Given a program, analyze a given routine with the given memory model and
+/// disassembler and populate the database with the results.
+/// 
+/// This function exists to isolate as much as possible of the top-level scan
+/// implementation from generic typing concerns. You call it with a compatible
+/// disassembler and memory and the database gets updated as you wished.
+/// 
+/// TODO: The current set of lifetime bounds preclude the use of zero-copy
+/// deserialization. We should figure out a way around that.
+fn scan_for_arch<I, SI, F, P, MV, S, IO, DIS, IMP>(prog: &project::Program, start_spec: &str, disassembler: &DIS,
+    bus: &memory::Memory<P, MV, S, IO>, importer: Option<&IMP>) -> io::Result<()>
+    where for <'dw> P: memory::PtrNum<S> + analysis::Mappable + cli::Nameable + serde::Deserialize<'dw> + serde::Serialize,
+        for <'dw> S: memory::Offset<P> + Debug + serde::Deserialize<'dw> + serde::Serialize,
+        DIS: Fn(&memory::Pointer<P>, &memory::Memory<P, MV, S, IO>) -> (Option<ast::Instruction<I, SI, F, P>>, S, bool, bool, Vec<analysis::Reference<P>>),
+        IMP: Fn(io::BufReader<fs::File>, &mut analysis::Database<P, S>) -> io::Result<()> {
+    let image = prog.iter_images().next().ok_or(io::Error::new(io::ErrorKind::Other, "Did not specify an image"))?;
+    let mut file = fs::File::open(image)?;
+
+    let mut pjdb = match project::ProjectDatabase::read(prog.as_database_path()) {
+        Ok(pjdb) => pjdb,
+        Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
+            eprintln!("Creating new database for project");
+            project::ProjectDatabase::new()
+        },
+        Err(e) => return Err(e)
+    };
+
+    let mut db = pjdb.get_database_mut(prog.as_name().expect("Projects must be named!"));
+    db.update_indexes();
+
+    if let Some(importer) = importer {
+        db.import_symbols(prog, importer)?;
     }
+
+    let start_pc = input::parse_ptr(start_spec, db, bus).expect("Must specify a valid address to analyze");
+    scan_pc_for_arch(&mut db, &start_pc, disassembler, bus);
 
     pjdb.write(prog.as_database_path())?;
 
