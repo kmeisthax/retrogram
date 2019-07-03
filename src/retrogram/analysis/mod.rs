ea730a719312f18a1e2650a23fbb74b25fa57db0
David Wendt <dcrkid@yahoo.com>
2019-07-03 13:30:41 -0600

    Struct type to store execution history throughout a program.

--- src/retrogram/analysis/mod.rs
+++ src/retrogram/analysis/mod.rs
@@ -4,6 +4,7 @@ mod passes;
 mod traits;
 mod reference;
 mod block;
+mod trace;
 
 pub use traits::*;
 pub use passes::disassemble_block;
@@ -12,3 +13,4 @@ pub use passes::inject_labels;
 pub use reference::ReferenceKind;
 pub use reference::Reference;
 pub use block::Block;
+pub use trace::Trace;
