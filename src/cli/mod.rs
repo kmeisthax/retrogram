6e151d8b3068bcb25aeca20f76352ad1ea25de08
David Wendt <dcrkid@yahoo.com>
2019-07-14 19:40:45 -0400

    Hoist the cli main into a module.

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -4,8 +4,34 @@ mod dis;
 mod scan;
 mod import;
 mod traits;
+mod main;
 
 pub use dis::dis;
 pub use scan::scan;
 pub use import::import;
 pub use traits::*;
+pub use main::main;
+
+use std::str;
+
+/// Enumeration of all CLI commands
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub enum Command {
+    Scan,
+    Disassemble,
+    Import
+}
+
+impl str::FromStr for Command {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_ascii_lowercase().as_ref() {
+            "dis" => Ok(Command::Disassemble),
+            "scan" => Ok(Command::Scan),
+            "chadtronic-scan" => Ok(Command::Scan),
+            "import" => Ok(Command::Import),
+            _ => Err(())
+        }
+    }
+}
