99bd03c8e7ff1e2ac98727382d6fa21b0b14340f
David Wendt <dcrkid@yahoo.com>
2020-05-24 20:16:55 -0400

    Register sets and memory writes should retain the symbolic values written to the state in the trace log.
    This also adds a convenience function to the existing trace log methods: they now write the state for you. `memory_write` will even allow you to write multiple bytes at once to the state.

--- src/analysis/trace.rs
+++ src/analysis/trace.rs
@@ -1,22 +1,26 @@
 //! Analysis trace
 
 use crate::memory::Pointer;
+use crate::reg::{State, Symbolic};
+use num_traits::One;
+use std::hash::Hash;
+use std::ops::Add;
 
 /// An individual event in the trace log.
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq)]
 pub enum TraceEvent<RK, RV, P, MV> {
     /// The execution of an instruction at a particular address.
     Execute(Pointer<P>),
 
     /// The setting of a register to a new value.
-    RegisterSet(RK, RV),
+    RegisterSet(RK, Symbolic<RV>),
 
     /// The writing of one or more bytes of memory to a particular location.
     ///
     /// This allows wider writes to be listed as a single operation. Writes
     /// should always be notated from the lowest address up, regardless of
     /// endianness.
-    MemoryWrite(Pointer<P>, Vec<MV>),
+    MemoryWrite(Pointer<P>, Vec<Symbolic<MV>>),
 }
 
 /// A Trace is a structure which stores the path that a dynamic analysis pass,
@@ -25,7 +29,7 @@ pub enum TraceEvent<RK, RV, P, MV> {
 /// This structure does not store execution state information; for that, you
 /// will need to consult `reg::State`. The Trace only contains information on
 /// how we got to that state.
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq)]
 pub struct Trace<RK, RV, P, MV> {
     /// Every instruction execution, register set, and memory write along the
     /// trace path.
@@ -49,15 +53,6 @@ impl<RK, RV, P, MV> Trace<RK, RV, P, MV> {
         self
     }
 
-    /// Add a new register set to the trace.
-    ///
-    /// This should be called *after* an execution trace (see `traced_to`).
-    /// Events before the first trace may not be displayed to users.
-    pub fn register_set(&mut self, reg: RK, val: RV) -> &mut Self {
-        self.log.push(TraceEvent::RegisterSet(reg, val));
-        self
-    }
-
     /// Read out the contents of the trace.
     pub fn iter(&self) -> impl Iterator<Item = &TraceEvent<RK, RV, P, MV>> {
         self.log.iter()
@@ -66,14 +61,60 @@ impl<RK, RV, P, MV> Trace<RK, RV, P, MV> {
 
 impl<RK, RV, P, MV> Trace<RK, RV, P, MV>
 where
+    RK: Eq + Hash + Clone,
+    RV: Clone,
+    P: Eq + Hash,
+{
+    /// Add a new register set to the trace.
+    ///
+    /// This should be called *after* an execution trace (see `traced_to`).
+    /// Events before the first trace may not be displayed to users.
+    ///
+    /// This function will also actually set the register value on the state.
+    pub fn register_set(
+        &mut self,
+        reg: RK,
+        val: Symbolic<RV>,
+        state: &mut State<RK, RV, P, MV>,
+    ) -> &mut Self {
+        self.log
+            .push(TraceEvent::RegisterSet(reg.clone(), val.clone()));
+
+        state.set_register(reg, val);
+
+        self
+    }
+}
+
+impl<RK, RV, P, MV> Trace<RK, RV, P, MV>
+where
+    RK: Eq + Hash,
+    P: Eq + Hash + Clone + One,
+    Pointer<P>: Clone,
     MV: Clone,
+    Pointer<P>: Add<P> + From<<Pointer<P> as Add<P>>::Output>,
 {
     /// Add a new memory write to the trace.
     ///
     /// This should be called *after* an execution trace (see `traced_to`).
     /// Events before the first trace may not be displayed to users.
-    pub fn memory_write(&mut self, ptr: Pointer<P>, val: &[MV]) -> &mut Self {
-        self.log.push(TraceEvent::MemoryWrite(ptr, val.to_vec()));
+    ///
+    /// This function will also actually write the memory value to the state.
+    pub fn memory_write(
+        &mut self,
+        mut ptr: Pointer<P>,
+        val: &[Symbolic<MV>],
+        state: &mut State<RK, RV, P, MV>,
+    ) -> &mut Self {
+        self.log
+            .push(TraceEvent::MemoryWrite(ptr.clone(), val.to_vec()));
+
+        for mv in val {
+            state.set_memory(ptr.clone(), mv.clone());
+
+            ptr = Pointer::from(ptr + P::one());
+        }
+
         self
     }
 }
