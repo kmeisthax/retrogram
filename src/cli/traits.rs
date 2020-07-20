948d68940bd0d4e6e3bc5f9be1675ca9121cd19e
David Wendt <dcrkid@yahoo.com>
2020-07-19 20:22:25 -0400

    Remove unnecessary `TryFrom<u64>` bound on `Nameable`.

--- src/cli/traits.rs
+++ src/cli/traits.rs
@@ -4,34 +4,28 @@ use std::convert::{TryFrom, TryInto};
 use std::fmt::{Debug, Display, LowerHex, UpperHex};
 use std::str::FromStr;
 
-/// Guard trait for pointer values users can input to us and we can output.
+/// Guard trait for values users can input to us and we can output.
 ///
 /// Effectively, this covers all of the operations we need to do in order for
 /// both the program and it's users to be able to name locations within a
-/// program.
+/// program. Hence, "nameable".
 ///
 /// The trait bounds, in plain english, require that we can:
 ///
 ///  * Parse the value from a string
-///  * Display the value, with upper or lowercase hexdecimal notation if needed
-///  * Attempt to convert the value from a u64 (for user input contexts)
-///  * Attempt to convert the value to and from a u8, so that `UpperHex` and
-///    `LowerHex` implementations on `Symbolic` can print the value four bits
-///    at a time. (We don't have a u4 impl, so u8 is the best option.)
+///  * Display the value, in a handful of different Rust-supported formatting
+///    modes.
+///  * Attempt to convert the value to and from a u8. This is to facilitate
+///    displaying symbolic versions of this value, as some of the traits
+///    mentioned here can only be symbolically accessed with a valid u8
+///    representation. Note that this will only be expected to be able to
+///    convert values up to 16, as it will always be first masked off with 0xF.
 pub trait Nameable:
-    Clone + Debug + FromStr + Display + LowerHex + UpperHex + TryFrom<u64> + TryFrom<u8> + TryInto<u8>
+    Clone + Debug + FromStr + Display + LowerHex + UpperHex + TryFrom<u8> + TryInto<u8>
 {
 }
 
 impl<T> Nameable for T where
-    T: Clone
-        + Debug
-        + FromStr
-        + Display
-        + LowerHex
-        + UpperHex
-        + TryFrom<u64>
-        + TryFrom<u8>
-        + TryInto<u8>
+    T: Clone + Debug + FromStr + Display + LowerHex + UpperHex + TryFrom<u8> + TryInto<u8>
 {
 }
